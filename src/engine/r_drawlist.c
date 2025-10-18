906f2580f0a8cc7db1fd9d3e8a4492b65947ee42
Evgeniy Mineev <e.mineev@crazypandagames.com>
2025-10-18 19:03:05 +0400

    fix compilation engine for Android

--- src/engine/r_drawlist.c
+++ src/engine/r_drawlist.c
@@ -215,11 +215,17 @@ void DL_ProcessDrawList(int tag, boolean(*procfunc)(vtxlist_t*, int*)) {
             }
             else {
                 unsigned int flags = ((visspritelist_t*)head->data)->spr->flags;
+#ifdef ANDROID
+                unsigned int packed = (unsigned int)head->texid;
+
+                palette = (int)((packed >> 24) & 0xFF);
+                head->texid = (int)(packed & 0xFFFF);
+#else
                 unsigned int __packed = (unsigned int)head->texid;
 
                 palette = (int)((__packed >> 24) & 0xFF);
                 head->texid = (int)(__packed & 0xFFFF);
-
+#endif
                 GL_BindSpriteTexture(head->texid, palette);
 
                 dglTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, (int)(r_objectFilter.value == 0 && r_filter.value > 0) ? GL_LINEAR : GL_NEAREST);
