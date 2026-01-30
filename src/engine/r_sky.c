a44f5208a1a37f56a5b91cd2ab8601c757a629c6
Styd051 <thierus.dylan@gmail.com>
2026-01-30 04:28:48 +0100

    Fixes a problem where the gradient color in the background of the clouds extends to the bottom of the screen instead of stopping halfway up.

--- src/engine/r_sky.c
+++ src/engine/r_sky.c
@@ -525,7 +525,7 @@ static void R_DrawClouds(void) {
     dglSetVertex(v);
 
     I_ShaderUnBind();
-    GL_Set2DQuad(v, 0, 0, SCREENWIDTH, SCREENHEIGHT, 0, 0, 0, 0, 0);
+    GL_Set2DQuad(v, 0, 0, SCREENWIDTH, 120, 0, 0, 0, 0, 0);
     dglSetVertexColor(&v[0], PostProcessSkyColor(sky->skycolor[1], true), 4);
     dglSetVertexColor(&v[2], PostProcessSkyColor(sky->skycolor[0], false), 2); // bottom
     dglDisable(GL_TEXTURE_2D);
@@ -541,7 +541,7 @@ static void R_DrawClouds(void) {
     dglSetVertexColor(&v[0], sky->skycolor[2], 4);
 
     I_ShaderUnBind();
-    GL_Set2DQuad(v, 0, 0, SCREENWIDTH, SCREENHEIGHT, 0, 0, 0, 0, 0);
+    GL_Set2DQuad(v, 0, 0, SCREENWIDTH, 120, 0, 0, 0, 0, 0);
     dglSetVertexColor(&v[0], PostProcessSkyColor(sky->skycolor[1], true), 4);
     dglSetVertexColor(&v[2], PostProcessSkyColor(sky->skycolor[0], false), 2);
 
