724700b66bc94085484673f3b4885be5d8b0aa3d
sandstranger <sandstranger@gmail.com>
2026-03-22 17:40:18 +0400

    downgrade gles 3.x to 3.0

--- src/engine/i_video.c
+++ src/engine/i_video.c
@@ -254,10 +254,10 @@ void I_InitScreen(void) {
 #endif
 #else
     SDL_Log(g_useGLES2_0 ? "Legacy OpenGL ES 2.0 is using for rendering" :
-            "OpenGL ES 3.1 is using for rendering");
+            "OpenGL ES 3.0 is using for rendering");
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, g_useGLES2_0 ? 2 : 3);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, g_useGLES2_0 ? 0 : 1);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION,   0);
 #endif
 
     SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
