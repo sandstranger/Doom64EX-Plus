da484877061942900f216a643e9deb12b2797a03
sandstranger <sandstranger@gmail.com>
2026-03-22 11:23:56 +0400

    always use gles 3.1 for angle emulation

--- src/engine/i_video.c
+++ src/engine/i_video.c
@@ -254,10 +254,10 @@ void I_InitScreen(void) {
 #endif
 #else
     SDL_Log(g_useGLES2_0 ? "Legacy OpenGL ES 2.0 is using for rendering" :
-            "OpenGL ES 3.2 is using for rendering");
+            "OpenGL ES 3.1 is using for rendering");
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, g_useGLES2_0 ? 2 : 3);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, g_useGLES2_0 ? 0 : 2);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, g_useGLES2_0 ? 0 : 1);
 #endif
 
     SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
